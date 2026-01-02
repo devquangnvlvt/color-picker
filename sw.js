09082a2526076cb90502482fd790719e3139a8ff
DESKTOP-L8PGEV1\Admin <quangnv@lvtapp.com>
2026-01-02 15:36:30 +0700

    up

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pixel-art-v22';
+const CACHE_NAME = 'pixel-art-v23';
 const ASSETS = [
     './',
     './index.html',
@@ -14,10 +14,35 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
     );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+        })
+    );
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
-    );
+    const url = new URL(e.request.url);
+
+    // Use Network-First for HTML to ensure latest version
+    if (url.pathname.endsWith('/') || url.pathname.endsWith('index.html')) {
+        e.respondWith(
+            fetch(e.request)
+                .then(res => {
+                    const clone = res.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+                    return res;
+                })
+                .catch(() => caches.match(e.request))
+        );
+    } else {
+        // Cache-First for other assets
+        e.respondWith(
+            caches.match(e.request).then(response => response || fetch(e.request))
+        );
+    }
 });
